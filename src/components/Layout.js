bed1f138a041bbc1d851d98bacafb3ce23ac7a93
Hon Boey <hon@dialogicstudios.com.au>
2021-08-19 16:42:23 +1000

    Styled width of page wrappers

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,7 +14,6 @@ function Layout({ children, title }) {
             }
         }      
     `)
-        console.log(title)
     return (
         <>
             <Helmet>
@@ -22,34 +21,36 @@ function Layout({ children, title }) {
                 <meta name="Keywords" content="Titirangi, Medical, Centre, health, Waitakere, doctor, gp, nurse, Auckland, medicine," />
                 <meta name="Description" content="For over seventy years, the Titirangi Medical Centre has supported our local community – keeping us healthy, safe and strong." />
             </Helmet>
-            <header>
-                <nav className="bg-tmc-green">
-                    <h1 className="font-display"><Link to="/"><StaticImage src="../images/logo.jpg" />Titirangi Medical Centre</Link></h1>
-                    <ul>
-                        <li><Link to="/bookings">Bookings & Enrolments</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/fees">Fees</Link></li>
-                        <li><Link to="/news">News & Information</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                {children}
-            </main>
-            <footer>
-                <h1><StaticImage src="../images/logo.jpg" /></h1>
-                <p>Mon – Fri<br />
-                    8am – 5pm</p>
-                <p>2 Rangiwai Rd<br />
-                    Titirangi<br />
-                    Waitakere 0604</p>
-                <p>PO Box 60107<br />
-                    Titirangi<br />
-                    Waitakere 0642</p>
-                <p>Phone – (09) 817 8069</p>
-                <p>Fax – (09) 817 8067</p>
-            </footer>
+            <div className="wrapper max-w-screen-xl mx-auto">
+                <header>
+                    <nav>
+                        <h1 className="font-display"><Link to="/"><StaticImage src="../images/logo.jpg" className="w-1/12"/>Titirangi Medical Centre</Link></h1>
+                        <ul>
+                            <li><Link to="/bookings">Bookings & Enrolments</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/fees">Fees</Link></li>
+                            <li><Link to="/news">News & Information</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
+                    {children}
+                </main>
+                <footer>
+                    <h1><StaticImage src="../images/logo.jpg" /></h1>
+                    <p>Mon – Fri<br />
+                        8am – 5pm</p>
+                    <p>2 Rangiwai Rd<br />
+                        Titirangi<br />
+                        Waitakere 0604</p>
+                    <p>PO Box 60107<br />
+                        Titirangi<br />
+                        Waitakere 0642</p>
+                    <p>Phone – (09) 817 8069</p>
+                    <p>Fax – (09) 817 8067</p>
+                </footer>
+            </div>
         </>
     )
 }
