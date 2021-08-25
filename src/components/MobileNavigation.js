5fe2a31c5a25678d29ad751df943f064ecdf140b
Hon Boey <hon@dialogicstudios.com.au>
2021-08-25 16:56:19 +1000

    removed logo from MobileNav

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -22,17 +22,11 @@ function MobileNavigation() {
             <nav className={navOpen ?
                 "bg-black h-screen w-screen z-10 fixed inset-0 p-4 transition-all duration-300 " :
                 "bg-black h-screen w-screen z-10 fixed inset-0 p-4 transition-all  duration-300 left-full"}>
-                <div class="flex justify-between">
-                    <button onClick={toggleNav} className="flex w-3/4 items-center mb-4">
-                        <Link to="/" className="w-1/6 mr-4"><StaticImage src="../images/logo-rev.png" alt="" /></Link>
-                        <Link to="/" className="no-underline text-tmc-green"><h1 className="font-display md:text-xl">Titirangi Medical Centre</h1></Link>
-                    </button>
-                    <button onClick={toggleNav} className="mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="#f4f9f1">
-                            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                        </svg>
-                    </button>
-                </div>
+                <button onClick={toggleNav} className="mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="#f4f9f1">
+                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                    </svg>
+                </button>
                 <ul className="text-tmc-green font-display text-2xl leading-loose">
                     <li><button onClick={toggleNav}><Link to="/" className="no-underline">Home</Link></button></li>
                     <li><button onClick={toggleNav}><Link to="/about" className="no-underline">About</Link></button></li>
@@ -42,7 +36,7 @@ function MobileNavigation() {
                     <li><button onClick={toggleNav} className="border border-tcm-green mt-4 rounded-md px-2 py-1"><Link to="/bookings" className="no-underline">Bookings & Enrolments</Link></button></li>
                 </ul>
             </nav>
-        </div>
+        </div >
     )
 }
 
