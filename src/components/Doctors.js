fc2eb71df5b61f4383cbb4bcdaa0752f665dfa19
Hon Boey <hon@dialogicstudios.com.au>
2021-09-02 09:30:20 +1000

    Added alt to doctor profile image

--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -4,7 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 function Doctors(props) {
     return (
         <li className="mb-12 lg:w-4/12 px-1/24">
-            <StaticImage className="mb-4 w-1/2 lg:w-full" src="../images/profile.jpg" />
+            <StaticImage className="mb-4 w-1/2 lg:w-full" src="../images/profile.jpg" alt="The doctor" />
             <h4 className="font-display text-2xl md:text-3xl mb-4">{props.name}</h4>
             <p className="uppercase leading-snug text-gray-500 mb-4">{props.position}<br />
                 {props.qualifications}</p>
