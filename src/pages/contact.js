e28b62b6079da2d755256ae03890c24307f0833b
Hon Boey <hon@dialogicstudios.com.au>
2021-09-01 15:12:51 +1000

    Styled Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,27 +5,37 @@ import Layout from "../components/Layout"
 function ContactPage() {
     return (
         <Layout title=" | Contact">
-            <section>
-                <h2>Contact</h2>
-                <h3>Street address</h3>
-                <a href="https://goo.gl/maps/b1cxbq87KPzqd18k6">
-                    <p>2 Rangiwai Rd<br/>
-                    Titrangi<br/>
-                    Waitakere 0604</p>
-                </a>
-                <StaticImage src="../images/map.png" alt="Google map" />
-                <h3>Phone</h3>
-                <a href="tel:"><p>(09) 817 8069</p></a>
-                <h3>Postal address</h3>
-                <p>PO Box 60107<br/>
-                Titirangi<br/>
-                Waitakere 0642</p>
-            </section>
-            <section>
-                <h2>Clinic hours</h2>
-                <h3>Mon – Fri</h3>
-                <p>8am – 5pm</p>
-            </section>
+            <div className="wrapper max-w-screen-xl mx-auto p-4 lg:p-8">
+                <section className="border-l border-r border-black px-1/24 py-8 mb-8 lg:flex">
+                    <div className="lg:w-1/2 lg:pr-1/24 lg:border-r lg:border-black">
+                        <h2 className="font-display text-4xl md:text-6xl leading-snug md:leading-normal">Contact</h2>
+                    </div>
+                    <div className="lg:w-1/2 lg:pl-1/24">
+                        <h3 className="uppercase leading-snug text-gray-500">Street Address</h3>
+                        <p className="font-display text-2xl md:text-3xl mb-4">2 Rangiwai Rd<br />
+                            Titrangi<br />
+                            Waitakere 0604</p>
+                        <StaticImage src="../images/map.png" alt="Google map" className="mb-8"/>
+                        <h3 className="uppercase leading-snug text-gray-500">Postal Address</h3>
+                        <p className="font-display text-2xl md:text-3xl mb-8">2 Rangiwai Rd<br />
+                            Titrangi<br />
+                            Waitakere 0604</p>
+                        <h3 className="uppercase leading-snug text-gray-500">Phone</h3>
+                        <p className="font-display text-2xl md:text-3xl mb-8">(09) 817 8069</p>
+                        <h3 className="uppercase leading-snug text-gray-500">Fax</h3>
+                        <p className="font-display text-2xl md:text-3xl mb-8">(09) 817 8069</p>
+                    </div>
+                </section>
+                <section className="border-l border-r border-black px-1/24 py-8 mb-8 lg:flex">
+                    <div className="lg:w-1/2 lg:pr-1/24 lg:border-r lg:border-black">
+                        <h2 className="font-display text-4xl md:text-6xl leading-snug md:leading-normal">Clinic Hours</h2>
+                    </div>
+                    <div className="lg:w-1/2 lg:pl-1/24">
+                        <h3 className="uppercase leading-snug text-gray-500">Monday – Friday</h3>
+                        <p className="font-display text-2xl md:text-3xl mb-4">8:00am – 5:00pm</p>
+                    </div>
+                </section>
+            </div>
         </Layout>
     )
 }
