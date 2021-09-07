e39bd8d1cf1f44867966c8152771bad911ddde2a
Hon Boey <hon@dialogicstudios.com.au>
2021-09-07 21:38:35 +1000

    Created 404 page. Fixed styling of News. Changed About content structure

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,22 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+function PageNotFound() {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout title="Page not found">
+      <div className="wrapper max-w-screen-xl mx-auto p-4 lg:p-8">
+        <section className="border-r border-l border-black pl-1/24 pr-1/24 mb-8">
+          <div className="py-8 lg:flex">
+            <h2 className="font-display text-4xl md:text-6xl leading-snug md:leading-normal lg:w-8/12 lg:border-r lg:border-black lg:pr-1/24 mb-8">Sorry – we haven't been able to find the page you were after.</h2>
+            <div className="lg:w-4/12 lg:px-1/24 lg:pt-8">
+              <p>Please use the navigation menu above to get to the correct page, otherwise drop us a line at <a href="mailto:admin@titirangimc.co.nz">admin@titirangimc.co.nz</a> if you can't find the information you were looking for.</p>
+            </div>
+          </div>
+        </section> 
+      </div>
+    </Layout>
   )
 }
 
-export default NotFoundPage
+export default PageNotFound
+
