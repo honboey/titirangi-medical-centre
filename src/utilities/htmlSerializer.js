6eb02ecba2167c05b44e4dcf0bd0695dbb9d2e13
Hon Boey <hon@dialogicstudios.com.au>
2021-09-08 07:32:45 +1000

    Fixed LinkResolver import syntax in htmlSerializer. LinkResolver is now working

--- src/utilities/htmlSerializer.js
+++ src/utilities/htmlSerializer.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import { Elements } from 'prismic-richtext'
 import { Link as PrismicLink } from 'prismic-reactjs'
-import linkResolver from './linkResolver'
+import { linkResolver } from './linkResolver'
 
 
 // -- HTML Serializer
