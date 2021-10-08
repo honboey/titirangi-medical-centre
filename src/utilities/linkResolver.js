55c373f070eafc53c6dca2d6917ccfa702e09dca
Hon Boey <hon@dialogicstudios.com.au>
2021-10-08 13:40:44 +1100

    Edited linkResolver to include links to all pages

--- src/utilities/linkResolver.js
+++ src/utilities/linkResolver.js
@@ -1,9 +1,8 @@
 // linkResolver.js file
 
 exports.linkResolver = (doc) => {
-  if (doc.type === 'about') {
+  if (doc.type === 'about' || 'news_and_informations' || 'fees') {
     return `/${doc.uid}`
-  }
-
+  } 
   return '/'
 }
