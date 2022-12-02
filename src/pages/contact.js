20f5dc4f2aac28f85dff50fd937e89dec5d65238
Hon <55781260+honboey@users.noreply.github.com>
2022-12-02 15:19:40 +1100

    Format email on Contact Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -22,10 +22,10 @@ function ContactPage() {
                             Titirangi<br />
                             Waitakere 0642</p>
                         <h3 className="uppercase leading-snug text-gray-500">Email (admin)</h3>
-                        <p><a  className="font-display text-2xl md:text-3xl mb-8" href="mailto:office@titirangimc.co.nz">
+                        <p className="font-display text-2xl md:text-3xl mb-8"><a href="mailto:office@titirangimc.co.nz">
                             office@titirangimc.co.nz</a></p>
                         <h3 className="uppercase leading-snug text-gray-500">Email (nurse)</h3>
-                        <p><a  className="font-display text-2xl md:text-3xl mb-8" href="mailto:nurse@titirangimc.co.nz">
+                        <p className="font-display text-2xl md:text-3xl mb-8" ><a href="mailto:nurse@titirangimc.co.nz">
                             nurse@titirangimc.co.nz</a></p>
                         <h3 className="uppercase leading-snug text-gray-500">Phone</h3>
                         <a href="tel:+64098178069">
